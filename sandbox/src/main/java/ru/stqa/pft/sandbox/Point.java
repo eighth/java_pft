2d956a8b9cbeb409f19a2cbce0b97b24f4996ce8
Igor Nechepurenko <ne4epurenko@gmail.com>
2017-04-12 23:09:42 +0300

    Добавил многострочный комментарий

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -16,5 +16,21 @@ public class Point {
   public static double distance(Point p1, Point p2) {
     return Math.sqrt(Math.pow(p2.x - p1.x, 2) + (Math.pow(p2.y - p1.y, 2)));
   }
+  /**
+   * Даже если я меняю метода:
+   * public double distance() {
+   * внутри указываю Math.sqrt(Math.pow(this.x - this.x, 2) + (Math.pow(this.y - this.y, 2)));
+   * И в main указываю:
+   * Point p1 = new Point(10, 2);
+   * Point p2 = new Point(4, 12);
+   *
+   * Всеравно не догоняю как можно передать p1.distance() и p2.distance(). А т.к. создан конструктор,
+   * то я не могу просто создать объект, назвать его Point p и передавать его p.distance();
+   *
+   * System.out.println("Расстояния между двумя точками = " + distance());
+   *
+   * Полюбому где-то я тууплю...
+   *
+   */
 
 }
