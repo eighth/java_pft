f8d3b742af538447b336c651cd47ff014083287e
Igor Nechepurenko <ne4epurenko@gmail.com>
2017-04-12 21:10:05 +0300

    Создан класс Point с конструктором

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Nechepurenko on 12.04.2017.
+ */
+
+public class Point {
+  public double x;
+  public double y;
+
+  public Point(double x, double y){
+    this.x = x;
+    this.y = y;
+  }
+}
+
+
+
+/**
+ *
+ *
+ *
+ * 4.123105625617661
+ *
+ *
+ *  Point p1 = new Point();
+ Point p2 = new Point();
+
+ p1.x = 5;
+ p1.y = 8;
+ p2.x = 9;
+ p2.y = 7;
+ *
+ *
+ System.out.println(Math.sqrt(Math.pow(p2.x - p1.x, 2) + (Math.pow(p2.y - p1.y, 2))));
+ *
+ */
