aa95362890549d396bc28f92290310913859a4ad
Igor Nechepurenko <ne4epurenko@gmail.com>
2017-04-12 21:45:25 +0300

    static double distance

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,33 +2,25 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-//
-//    hello("world");
-//    hello("user");
-//    hello("Igor");
-//
-//    //Создаем объект типа квадрат
-//    Square s = new Square(5);
-//    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
-//
-//    Rectangle r = new Rectangle(4, 6);
-//    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
-//
-//  }
-//
-//  public static void hello(String somebody) {
-//    System.out.println("Hello, " + somebody + "!!!");
-//  }
-//  }
+    hello("world");
+    hello("user");
+    hello("Igor");
 
-    //Создаем объекты типа Point(у point есть конструктор)
+    //Создаем объект типа квадрат
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+    //Создаем объект типа Point
     Point p1 = new Point(10, 2);
     Point p2 = new Point(4, 12);
+    System.out.println("Расстояния между двумя точками = " + Point.distance(p1, p2));
 
-    System.out.println("Между = " + Point.distance(p1, p2));
   }
 
-//  public static double distance(Point p1, Point p2) {
-//    return Math.sqrt(Math.pow(p2.x - p1.x, 2) + (Math.pow(p2.y - p1.y, 2)));
-//  }
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!!!");
+  }
 }
