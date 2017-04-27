41e419826201c29c1322608dfbeab1450fe38a77
Igor Nechepurenko <ne4epurenko@gmail.com>
2017-04-27 23:37:08 +0300

    Циклы

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PrimeTests.java
@@ -0,0 +1,30 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Nechepurenko on 27.04.2017.
+ */
+public class PrimeTests {
+  @Test
+  public void testPrime(){
+    Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
+  }
+
+  @Test
+  public void testNonPrime(){
+    Assert.assertFalse(Primes.isPrime(Integer.MAX_VALUE - 2));
+  }
+
+  @Test(enabled = false)
+  public void testPrimeLong(){
+    long n = Integer.MAX_VALUE;
+    Assert.assertTrue(Primes.isPrime(n));
+  }
+
+  @Test
+  public void testPrimeFast(){
+    Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
+  }
+}
