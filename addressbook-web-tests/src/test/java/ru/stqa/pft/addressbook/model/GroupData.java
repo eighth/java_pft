0569c4817fdd3e912cb04ad523eca54062d1e653
Igor Nechepurenko <ne4epurenko@gmail.com>
2017-05-01 21:39:55 +0300

    Неупорядоченные коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,18 +1,23 @@
 package ru.stqa.pft.addressbook.model;
 
 public class GroupData {
+  private final String id;
   private final String name;
   private final String header;
   private final String footer;
 
   public GroupData(String name, String header, String footer) {
+    this.id = null;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
-  public String getName() {
-    return name;
+  public GroupData(String id, String name, String header, String footer) {
+    this.id = id;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
   }
 
   @Override
@@ -22,19 +27,27 @@ public class GroupData {
 
     GroupData groupData = (GroupData) o;
 
+    if (id != null ? !id.equals(groupData.id) : groupData.id != null) return false;
     return name != null ? name.equals(groupData.name) : groupData.name == null;
   }
 
   @Override
   public int hashCode() {
-    return name != null ? name.hashCode() : 0;
+    int result = id != null ? id.hashCode() : 0;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return result;
   }
 
   @Override
   public String toString() {
     return "GroupData{" +
-            "name='" + name + '\'' +
+            "id='" + id + '\'' +
+            ", name='" + name + '\'' +
             '}';
+  }
+
+  public String getName() {
+    return name;
   }
 
   public String getHeader() {
@@ -43,5 +56,9 @@ public class GroupData {
 
   public String getFooter() {
     return footer;
+  }
+
+  public String getId() {
+    return id;
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -22,11 +23,17 @@ public class GroupModificationTests extends TestBase {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test01", "test02", "test03"));
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(),"test01", "test02", "test03");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     //Список групп вместо количества
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() - 1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<>(before),new HashSet<>(after));
+
   }
 }
