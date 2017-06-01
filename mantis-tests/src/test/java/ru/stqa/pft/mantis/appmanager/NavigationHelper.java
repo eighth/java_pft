016883f73d34fa3a93cac0922473b6c6e8a64e4c
i.nechepurenko <i.nechepurenko@ntc-vulkan.ru>
2017-06-01 15:31:39 +0300

    reformat

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/NavigationHelper.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/NavigationHelper.java
@@ -10,24 +10,24 @@ public class NavigationHelper extends HelperBase {
     super(app);
   }
 
-  public void Users(){
+  public void Users() {
     click(By.cssSelector("a[href='/mantisbt-1.2.19/manage_user_page.php']"));
   }
 
-  public void clickUser(int id){
-    click(By.cssSelector("a[href='manage_user_edit_page.php?user_id="+id+"']"));
+  public void clickUser(int id) {
+    click(By.cssSelector("a[href='manage_user_edit_page.php?user_id=" + id + "']"));
   }
 
 
-  public void resetPassword(){
+  public void resetPassword() {
     click(By.cssSelector("input[value='Reset Password']"));
   }
 
 
   public void cheangePassword(String confirmationLink, String password) {
     wd.get(confirmationLink);
-    type(By.name("password"),password);
-    type(By.name("password_confirm"),password);
+    type(By.name("password"), password);
+    type(By.name("password_confirm"), password);
     click(By.cssSelector("input[value='UpdateUser']"));
   }
 
