2b4b756891d74c0f48457f98da9a593c4439f329
IgorNechepurenko <ne4epurenko@gmail.com>
2017-06-08 23:51:50 +0300

    Про рест.
    16 минута

--- rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
@@ -1,13 +1,40 @@
 package ru.stqa.pft.rest;
 
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Set;
 
 /**
  * Created by nigor on 08.06.2017.
  */
 public class RestTests {
   @Test
-  public void testCreateIssue() {
+  public void testCreateIssue() throws IOException {
+    Set<Issue> oldIssues = getIssues();
+    Issue newIssue = new Issue();
+    int issueId = createIssue(newIssue);
+    Set<Issue> newIssues = getIssues();
+    oldIssues.add(newIssue.withId(issueId));
+    Assert.assertEquals(newIssues, oldIssues);
+  }
 
+
+
+  private Set<Issue> getIssues() throws IOException {
+    String json = getExecutor().execute(Request.Get("http://example.bugifyapp.com/api/issues.json"))
+            .returnContent().asString();
+    return null;
+  }
+
+  private Executor getExecutor() {
+    return Executor.newInstance().auth("LSGjeU4yP1X493ud1hNniA==", "")
+  }
+
+  private int createIssue(Issue newIssue) {
+    return 0;
   }
 }
