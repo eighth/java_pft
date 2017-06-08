1adeb1571996fbd30298e26a4954021901cdb7ab
IgorNechepurenko <ne4epurenko@gmail.com>
2017-06-08 22:28:01 +0300

    Задание №21: Реализовать интеграцию тестов с баг-трекером MantisBT

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -1,12 +1,21 @@
 package ru.stqa.pft.mantis.tests;
 
+import biz.futureware.mantis.rpc.soap.client.IssueData;
+import biz.futureware.mantis.rpc.soap.client.MantisConnectLocator;
+import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import javax.xml.rpc.ServiceException;
 import java.io.File;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.rmi.RemoteException;
 
 /**
  * Created by i.nechepurenko on 30.05.2017.
@@ -26,5 +35,22 @@ public class TestBase {
   public void tearDown() throws IOException {
     app.ftp().restore("config_inc.php.bak", "config_inc.php");
     app.stop();
+  }
+
+  public boolean isIssueOpen(int issueId) throws RemoteException, MalformedURLException, ServiceException {
+    MantisConnectPortType mc = new MantisConnectLocator().getMantisConnectPort(new URL("http://localhost/mantisbt-1.2.19/api/soap/mantisconnect.php"));
+    IssueData fixedIssueData = mc.mc_issue_get("Administrator", "root", BigInteger.valueOf(issueId));
+    if (fixedIssueData.getResolution().getName().equals("fixed")){
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+
+  public void skipIfNotFixed(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+    if (isIssueOpen(issueId)) {
+      throw new SkipException("Ignored because of issue " + issueId);
+    }
   }
 }
