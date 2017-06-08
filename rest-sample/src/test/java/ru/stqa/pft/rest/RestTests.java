d18e521c679ec46894782dd8572ef7ccff5a9067
IgorNechepurenko <ne4epurenko@gmail.com>
2017-06-08 23:00:11 +0300

    Создан класс рест

---
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.rest;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by nigor on 08.06.2017.
+ */
+public class RestTests {
+  @Test
+  public void testCreateIssue() {
+
+  }
+}
